3306b3142a971272398c3a528ef2eb28fa96aab2
jorgehara <empresamodelo2018@gmail.com>
2023-02-08 11:21:18 -0300

    Delete scss

--- src/components/Modals/BasicModal.jsx
+++ src/components/Modals/BasicModal.jsx
@@ -5,7 +5,7 @@ const BasicModal = ({title, children, isOpen, onClose}) => {
     const modalContainer = document.querySelector("#modalContainer");
 return (
     isOpen ? ReactDOM.createPortal(
-        <div className='container-flex d-flex flex-column justify-content-start align-items-center modalClass '>
+        <div className='container-flex d-flex flex-column justify-content-start align-items-center backgroundModal'>
             <div className='row p-0 m-0 w-100 h-100 d-flex flex-column justify-content-center align-items-center'>                    
                 <div className='d-flex flex-column justify-content-center align-items-center '>
                     {children}
