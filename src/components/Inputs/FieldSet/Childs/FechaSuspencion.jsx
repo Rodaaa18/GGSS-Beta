5df802988d49071e5453dd2dc49ae97853a927f9
Rodrigo <rodrigo@inft.com.ar>
2023-02-24 16:14:47 -0300

    Update FechaSuspencion.jsx

--- src/components/Inputs/FieldSet/Childs/FechaSuspencion.jsx
+++ src/components/Inputs/FieldSet/Childs/FechaSuspencion.jsx
@@ -5,8 +5,8 @@ import "./Childs.css";
 
 
 const FechaSuspencion = ({valueForm, onChange, setCheckeds, setFormLicencias, formLicencias}) => {
-  const [checked, setChecked] = useState(false);
-  const [disable, setDisable] = useState(false);
+  const [checked, setChecked] = useState(true);
+  const [disable, setDisable] = useState(true);
 
   useEffect(()=>{
     setCheckeds(false)
@@ -18,7 +18,7 @@ const FechaSuspencion = ({valueForm, onChange, setCheckeds, setFormLicencias, fo
             <InputDate disabled={disable} nameInput="Fecha Suspención:" onChange={onChange} valueCheck={true} idInput="inputDateSuspLic" value={checked ? "" : valueForm?.inputDateSuspLic && valueForm?.inputDateSuspLic} />
             <div className='d-flex flex-row justify-content-start align-items-center'>
                 <div className='space'/>
-                <input type="checkbox" checked={checked} className='mt-1' name="inputQuitaSusp" id="inputQuitaSusp" onChange={(e)=>{ setDisable(!disable); setChecked(!checked); onChange(e.target.checked, "inputQuitaSusp"); setFormLicencias({...formLicencias, inputDateSuspLic : null, inputQuitaSusp : !checked })}} />
+                <input type="checkbox" checked={checked} defaultValue={false} className='mt-1' name="inputQuitaSusp" id="inputQuitaSusp" onChange={(e)=>{ setDisable(!disable); setChecked(!checked); onChange(e.target.checked, "inputQuitaSusp"); setFormLicencias({...formLicencias, inputDateSuspLic : null, inputQuitaSusp : !checked })}} />
                 <label htmlFor="inputQuitaSusp">Quitar Fecha de Suspensión</label>
             </div>
         </div>
